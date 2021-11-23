7a0bb0feb5b0d1a1d739c26b64c678ede4f308d4
Mohamed Mohamud <momo@Mohameds-MacBook-Pro.local>
2021-11-22 17:49:29 -0800

    last changes

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function generatePassword() {
   var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
 
-  while(confirmLength <= 7 || confirmLength >= 51) {
-      alert("Password length must be between 8-50 characters Try again");
+  while(confirmLength <= 7 || confirmLength >= 128) {
+      alert("Password length must be between 8-128 characters Try again");
       var confirmLength = (prompt("How many characters would you like your password to contain?"));
       } 
 
